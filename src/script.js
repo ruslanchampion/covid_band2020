ae2ca0cd1f789ec26a5b679c8229f5593d098dc1
ruslanchampion <ruslan.timbakov@gmail.com>
2020-12-25 22:06:18 +0800

    last push

--- src/script.js
+++ src/script.js
@@ -3,4 +3,3 @@ import Table from './js/Table';
 import './mapGraph/App';
 import './js/tableOne';
 new Table().handleMethods();
-alert('Пожалуйста если есть возможность проверьте позже, 26 или 27 числа!!!')
